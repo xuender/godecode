384846cf6dc382f317c3353757504a9ee7681647
徐晨阳 <xuender@gmail.com>
2014-10-03 22:01:54 +0800

    test bug

--- godecode.go
+++ godecode.go
@@ -1,79 +1,82 @@
 package godecode
 
 import (
-  "fmt"
-  "os"
-  "bufio"
-  "strings"
-  "regexp"
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
 )
 
 // Transliterate Unicode string to a valid 7-bit ASCII String.
 type GoDecode struct {
-  path  string
-  cache map[rune][]string
+	path  string
+	cache map[rune][]string
 }
+
 // Init data path
-func (gd *GoDecode) Init(path string){
-  gd.path = path
-  gd.cache = make(map[rune][]string)
+func (gd *GoDecode) Init(path string) {
+	gd.path = path
+	gd.cache = make(map[rune][]string)
 }
-func (gd *GoDecode) getCache(section rune) []string{
-  m, ok := gd.cache[section]
-  if ok {
-    return m
-  }
-  var path string
-  if os.IsPathSeparator('\\') {
-    path = "\\"
-  } else {
-    path = "/"
-  }
-  f, err := os.OpenFile(fmt.Sprintf("%s%sX%03x", gd.path, path, section),os.O_RDONLY,0660)
-  if err != nil {
-    panic(err)
-  }
-  gd.cache[section] = read(bufio.NewScanner(f))
-  return gd.cache[section]
+func (gd *GoDecode) getCache(section rune) []string {
+	m, ok := gd.cache[section]
+	if ok {
+		return m
+	}
+	var path string
+	if os.IsPathSeparator('\\') {
+		path = "\\"
+	} else {
+		path = "/"
+	}
+	f, err := os.OpenFile(fmt.Sprintf("%s%sX%03x", gd.path, path, section), os.O_RDONLY, 0660)
+	if err != nil {
+		panic(err)
+	}
+	gd.cache[section] = read(bufio.NewScanner(f))
+	return gd.cache[section]
 }
-func read(scanner *bufio.Scanner) []string{
-  var ret []string
-  for scanner.Scan(){
-    ret = append(ret, scanner.Text())
-  }
-  if scanner.Err() != nil {
-    panic(scanner.Err())
-  }
-  return ret
+func read(scanner *bufio.Scanner) []string {
+	var ret []string
+	for scanner.Scan() {
+		ret = append(ret, scanner.Text())
+	}
+	if scanner.Err() != nil {
+		panic(scanner.Err())
+	}
+	return ret
 }
+
 // Transliterate an Unicode object into an ASCII string.
 func (gd *GoDecode) Decode(str string) string {
-  var ret []string
-  for _, codepoint := range str {
-    // Basic ASCII
-    if codepoint < 0x80 {
-      ret = append(ret, string(codepoint))
-      continue
-    }
-    // Characters in Private Use Area and above are ignored
-    if codepoint > 0xffff {
-      continue
-    }
-    section := codepoint >> 8; // Chop off the last two hex digits
-    position := codepoint % 256; // Last two hex digits
-    table := gd.getCache(section);
-    if table != nil && rune(len(table)) > position {
-      ret = append(ret, table[position])
-    }
-  }
-  return strings.Trim(strings.Join(ret, ""), " ")
+	var ret []string
+	for _, codepoint := range str {
+		// Basic ASCII
+		if codepoint < 0x80 {
+			ret = append(ret, string(codepoint))
+			continue
+		}
+		// Characters in Private Use Area and above are ignored
+		if codepoint > 0xffff {
+			continue
+		}
+		section := codepoint >> 8   // Chop off the last two hex digits
+		position := codepoint % 256 // Last two hex digits
+		table := gd.getCache(section)
+		if table != nil && rune(len(table)) > position {
+			ret = append(ret, table[position])
+		}
+	}
+	return strings.Trim(strings.Join(ret, ""), " ")
 }
+
 // Transliterate Unicode string to a initials.
-func (gd *GoDecode) Initials(str string) string{
-  var ret []string
-  reg := regexp.MustCompile("^\\w|\\s+\\w")
-  for _, s := range reg.FindAllString(gd.Decode(str), -1){
-    ret = append(ret, strings.Replace(s, " ", "", -1))
-  }
-  return strings.Join(ret, "")
+func (gd *GoDecode) Initials(str string) string {
+	var ret []string
+	reg := regexp.MustCompile("^\\w|\\s+\\w")
+	for _, s := range reg.FindAllString(gd.Decode(str), -1) {
+		ret = append(ret, strings.Replace(s, " ", "", -1))
+	}
+	return strings.Join(ret, "")
 }
--- godecode_test.go
+++ godecode_test.go
@@ -1,31 +1,33 @@
 package godecode
+
 import "testing"
-func TestDecode(t *testing.T){
-  var gd GoDecode
-  gd.Init("data")
-  m := make(map[string]string)
-  m["hello world"] = "hello world"
-  m["南无阿弥陀佛"] = "Nan Wu A Mi Tuo Fo"
-  m["Κνωσός"] = "Knosos"
-  m["あみだにょらい"] = "amidaniyorai"
-  for k, v := range m {
-    if gd.Decode(k) != v {
-      t.Errorf("[ %s ] Error: [ %s ] != [ %s ]", k, v, gd.Decode(k))
-    }
-  }
+
+func TestDecode(t *testing.T) {
+	var gd GoDecode
+	gd.Init("data")
+	m := make(map[string]string)
+	m["hello world"] = "hello world"
+	m["南无阿弥陀佛"] = "Nan Wu A Mi Tuo Fo"
+	m["Κνωσός"] = "Knosos"
+	m["あみだにょらい"] = "amidaniyorai"
+	for k, v := range m {
+		if gd.Decode(k) != v {
+			t.Errorf("[ %s ] Error: [ %s ] != [ %s ]", k, v, gd.Decode(k))
+		}
+	}
 }
-func TestInitals(t *testing.T){
-  var gd GoDecode
-  gd.Init("data")
-  m := make(map[string]string)
-  m["Hello world."] = "Hw"
-  m["南无阿弥陀佛"] = "NWAMTF"
-  m["Κνωσός"] = "K"
-  m["あみだにょらい"] = "a"
-  m["小小姑娘\n清早起床\n\r提着花篮\t上市场。"] = "XXGN\nQZQC\n\rTZHL\tSSC"
-  for k, v := range m {
-    if gd.Initials(k) != v {
-      t.Errorf("[ %s ] Error: [ %s ] != [ %s ]", k, v, gd.Decode(k))
-    }
-  }
+func TestInitals(t *testing.T) {
+	var gd GoDecode
+	gd.Init("data")
+	m := make(map[string]string)
+	m["Hello world."] = "Hw"
+	m["南无阿弥陀佛"] = "NWAMTF"
+	m["Κνωσός"] = "K"
+	m["あみだにょらい"] = "a"
+	m["小小姑娘\n清早起床\n\r提着花篮\t上市场。"] = "XXGN\nQZQC\n\rTZHL\tSSC"
+	for k, v := range m {
+		if gd.Initials(k) != v {
+			t.Errorf("[ %s ] Error: [ %s ] != [ %s ]", k, v, gd.Initials(k))
+		}
+	}
 }
